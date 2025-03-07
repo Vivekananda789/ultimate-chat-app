ba3c9217d021625cf2953567c755eb00d8a1a419
Crazy haxers <111678641+Vivekananda789@users.noreply.github.com>
2025-03-07 18:28:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send
+from datetime import datetime
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    timestamp = datetime.now().strftime('%H:%M:%S')
+    message_with_time = f"[{timestamp}] {msg}"
+    send(message_with_time, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=10000)
