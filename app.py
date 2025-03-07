c343b08ca666d93eb12679b2969819a2e5aa29e6
Crazy haxers <111678641+Vivekananda789@users.noreply.github.com>
2025-03-07 19:18:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, join_room, leave_room, send
-from datetime import datetime
+from flask_socketio import SocketIO, send, join_room, leave_room
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+users_online = set()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('join')
-def on_join(data):
+def handle_join(data):
     username = data['username']
-    room = data['room']
+    room = data['room'] if data['room'] else 'general'
     join_room(room)
-    send({'msg': f"{username} has joined the room.", 'timestamp': datetime.now().strftime('%H:%M:%S')}, room=room)
-
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    send({'msg': f"{username} has left the room.", 'timestamp': datetime.now().strftime('%H:%M:%S')}, room=room)
+    users_online.add(username)
+    socketio.emit('server_message', {'msg': f'{username} has joined the room.'}, room=room)
+    socketio.emit('update_users', {'count': len(users_online), 'users': list(users_online)})
 
 @socketio.on('message')
-def handle_message(data):
-    data['timestamp'] = datetime.now().strftime('%H:%M:%S')
-    send(data, room=data['room'])
+def handle_message(msg):
+    socketio.emit('chat_message', msg, room=msg['room'])
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    for user in list(users_online):
+        users_online.remove(user)
+    socketio.emit('update_users', {'count': len(users_online), 'users': list(users_online)})
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
