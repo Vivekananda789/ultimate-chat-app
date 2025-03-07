0cd1ae909b754054b606c416d4380d9eb42b1915
Crazy haxers <111678641+Vivekananda789@users.noreply.github.com>
2025-03-07 20:42:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, join_room, leave_room
+from flask_socketio import SocketIO, join_room, leave_room, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# Track online users
-online_users = set()
+# Track online users and rooms
+online_users = {}
+rooms = {}
 
 @app.route('/')
 def index():
@@ -18,29 +19,51 @@ def handle_join(data):
     room = data.get('room', 'general')
 
     join_room(room)
-    online_users.add(username)
+    if room not in rooms:
+        rooms[room] = set()
+    rooms[room].add(username)
+    online_users[username] = room
 
-    # Notify room of new user
-    socketio.emit('user_joined', {'username': username, 'online_users': list(online_users)}, room=room)
-    socketio.emit('update_user_count', {'count': len(online_users)})
+    # Broadcast join event
+    emit('user_joined', {
+        'username': username,
+        'room': room,
+        'online_users': list(rooms[room])
+    }, room=room)
+
+    # Update user count
+    emit('update_user_count', {
+        'count': len(rooms[room]),
+        'online_users': list(rooms[room])
+    }, room=room)
 
 @socketio.on('leave')
 def handle_leave(data):
     username = data['username']
-    room = data.get('room', 'general')
+    room = online_users.get(username)
 
-    leave_room(room)
-    if username in online_users:
-        online_users.remove(username)
+    if room:
+        leave_room(room)
+        rooms[room].discard(username)
+        online_users.pop(username, None)
 
-    # Notify room of user leaving
-    socketio.emit('user_left', {'username': username, 'online_users': list(online_users)}, room=room)
-    socketio.emit('update_user_count', {'count': len(online_users)})
+        # Broadcast leave event
+        emit('user_left', {
+            'username': username,
+            'room': room,
+            'online_users': list(rooms[room])
+        }, room=room)
+
+        # Update user count
+        emit('update_user_count', {
+            'count': len(rooms[room]),
+            'online_users': list(rooms[room])
+        }, room=room)
 
 @socketio.on('message')
 def handle_message(data):
     room = data.get('room', 'general')
-    socketio.emit('message', data, room=room)
+    emit('message', data, room=room)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
